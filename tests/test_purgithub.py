ace8cb6e05be17b5fc48e2cc723793337392d1b4
Marco Tiraboschi <marcotiraboschi@hotmail.it>
2021-03-18 17:03:41 +0100

    purgithub quiet mode

--- tests/test_purgithub.py
+++ tests/test_purgithub.py
@@ -2,12 +2,23 @@
 import unittest
 import contextlib
 import io
-from chromatictools import purgithub
+from chromatictools import purgithub, unittestmixins
 
 
-class TestPurgithub(unittest.TestCase):
+class TestPurgithub(
+  unittestmixins.AssertPrintsMixin,
+  unittest.TestCase
+):
   """Test purgithub cli tool"""
   def test_purge_repo_home(self):
     """Test on repo home"""
     with io.StringIO() as buf, contextlib.redirect_stdout(buf):
       purgithub.main("https://github.com/ChromaticIsobar/chromatictools")
+
+  def test_purge_repo_home_quiet_mode(self):
+    """Test on repo home with quiet mode on"""
+    with self.assert_prints(""):
+      purgithub.main(
+        "--quiet",
+        "https://github.com/ChromaticIsobar/chromatictools"
+      )
